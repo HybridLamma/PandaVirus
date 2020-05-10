b72e91dc8a729f1d26050134746068cd3849787f
HybridLamma <64638047+HybridLamma@users.noreply.github.com>
2020-05-10 14:13:38 +0300

    Created a Scene for Testing TopDownCamera

---
+++ TestGit/Assets/Camera/Code/Scripts/TopDown_Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDown.Cameras
+{
+    public class TopDown_Camera : MonoBehaviour
+    {
+        public Transform m_Target;
+        public float n_Height = 10f;
+        public float n_Distance = 20f;
+        public float n_Angle = 45;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
